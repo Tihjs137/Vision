8a6d663964c7ada4ed4eeab69056e9b779d5ad76
Thijs <Thijsdejong21@gmail.com>
2017-11-24 20:41:49 +0100

    Now the program uses blobbing, an advanced algorithm that calculates the center of objects, in this case a filterd image. The terminal can be used for debug purposes

--- Opencv/Source.cpp
+++ Opencv/Source.cpp
@@ -4,20 +4,41 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
+
+#pragma comment(lib, "ComCtl32.lib")
+
 using namespace std;					// otherwise we need to use "std::" infront of all the std tasks
 using namespace cv;						// otherwise we need to use "cv::" infront of all the opencv tasks
 Mat Canny_picture;
 Mat Output;								//for finising step :diplaying ball on original image 
 Mat dst;								//For blurring
 char zBuffer[35];						//      "
+SimpleBlobDetector::Params params;
 SimpleBlobDetector detector;
+SimpleBlobDetector::Params Setup_blob();
+Mat blob(SimpleBlobDetector::Params params, Mat im);
+bool vision();
 
 
-int main(int, char**)					// create the main loop
+int main(int argc, char *argv[])
+{
+	vision();
+}
+
+
+
+
+
+
+bool vision()					// create the main loop
 {
 	VideoCapture cap(0);				// open the default camera, should you have a second external camera plugged in you can use "cap(1)" to take the camera
 	if (!cap.isOpened())				// check if we succeeded
 		return -1;						// if not we close the program
+
+	params = Setup_blob();
+
+
 
 	Mat frame;							// create the matrix in which the information frame of the camera will be safed
 										// create the windows in which to show your result (multiple options like size)
@@ -35,24 +56,27 @@ int main(int, char**)					// create the main loop
 											// Threshold the HSV image, keep only the color pixels you want. Maybe google what HSV means to see how to use it
 		inRange(hsv_image, Scalar(100, 100, 70), Scalar(140, 255, 255), ColorOnly); //
 
-		//namedWindow("ColorFilter", WINDOW_AUTOSIZE);
+		namedWindow("ColorFilter", WINDOW_AUTOSIZE);
 		imshow("ColorFilter", ColorOnly);			// show the HSV filter output in the window called "ColorFilter"
 		namedWindow("Input", WINDOW_AUTOSIZE);
 		imshow("Input", orig_image);		// show the camera picture in the window called "Input"
-
-		
 		
 		GaussianBlur(ColorOnly, dst, Size(35, 35), 0, 0);
 
-		//put the text in the "zBuffer" to the "dst" image
-		putText(dst, zBuffer, Point(ColorOnly.cols / 4, ColorOnly.rows / 8), CV_FONT_HERSHEY_COMPLEX, 1, Scalar(255, 255, 255));
-
+		//cout << "Before blob \n";
+		Mat blobs = blob(params, dst);
+		if (!blobs.empty())
+		{
+			imshow("Blob", blobs);
+		}
+		
+		
 		//show the blurred image with the text
-		//imshow("Smoothed Image", dst);
+		imshow("Smoothed Image", dst);
 				
 		Canny(dst, Canny_picture, 40, 60, 3);
 		namedWindow("Canny edge", WINDOW_AUTOSIZE);
-		//imshow("Canny edge", Canny_picture);
+		imshow("Canny edge", Canny_picture);
 
 
 		Output = orig_image.clone();
@@ -62,4 +86,60 @@ int main(int, char**)					// create the main loop
 		if (waitKey(30) >= 0) break;		// if you press any key while in the output window the program will close
 	}
 	return 0; 								// the camera will be deinitialized automatically in VideoCapture destructor
+}
+
+Mat blob(SimpleBlobDetector::Params params, Mat im)
+{
+	Mat dst;
+	bitwise_not(im, dst);
+	// Detect blobs.
+	std::vector<KeyPoint> keypoints;
+
+	Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+	detector->detect(dst, keypoints);
+
+	// Draw detected blobs as red circles.
+	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
+	Mat im_with_keypoints;
+	drawKeypoints(dst, keypoints, im_with_keypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	
+	
+	if ( keypoints.size() != 0)						//This loop fetches the locations from the keypoint data container
+	{
+		for (int i = 0; i < keypoints.size(); i++)
+		{
+			int x = keypoints[i].pt.x;
+			int y = keypoints[i].pt.y;
+
+			cout << "Found object number: " << i <<" at  x: " << x << ", y: " << y << "\n";  //here it is printed to the terminal
+		}
+	}
+
+	return im_with_keypoints;
+}
+
+SimpleBlobDetector::Params Setup_blob()
+{
+
+
+	// Change thresholds
+	params.minThreshold = 100;
+	params.maxThreshold = 200;
+
+	// Filter by Area.
+	params.filterByArea = false;
+	params.minArea = 1500;
+
+	// Filter by Circularity
+	params.filterByCircularity = false;
+	params.minCircularity = 0.1;
+
+	// Filter by Convexity
+	params.filterByConvexity = false;
+	params.minConvexity = 0.87;
+
+	// Filter by Inertia
+	params.filterByInertia = false;
+	params.minInertiaRatio = 0.01;
+	return params;
 }
